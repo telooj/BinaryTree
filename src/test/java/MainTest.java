5e2a7c1b99a04ad7eb3fd1ebdbedb9a6ca341178
Jurrian <Jurrian@L0758.vnog.intra>
2019-11-08 20:50:01 +0100

    Added execution right for gradle

--- src/test/java/MainTest.java
+++ src/test/java/MainTest.java
@@ -1,9 +1,11 @@
+import org.junit.Test;
 import static org.junit.Assert.*;
 
-class MainTest {
-    public static void main(String[] args) {
-        BinaryTree testTree = new BinaryTree();
+public class MainTest {
+    public BinaryTree testTree = new BinaryTree();
 
+    public void test() {
+        //fill testTree
         testTree.insert(8);
         testTree.insert(3);
         testTree.insert(12);
@@ -14,27 +16,49 @@ class MainTest {
         testTree.insert(16);
         testTree.insert(1);
         testTree.insert(10);
+    }
 
-        //Test 1: Als je de node met waarde 16 zoekt, krijg je 16 terug.
-        assertEquals(testTree.find(testTree.root, 16).toString(), "16");
-        //Test 2: Als je de node met waarde 16 zoekt, krijg je niet de node met een andere waarde terug.
-        assertNotEquals(testTree.find(testTree.root, 16).toString(), "15");
-        //Test 3: Als je de node met de hoogste waarde zoekt, krijg je de hoogste waarde terug.
-        assertEquals(testTree.findMax(testTree.root).toString(), "16");
-        //Test 4: Als je de node met de hoogste waarde zoekt, krijg je niet een node met een lagere waarde terug.
-        assertNotEquals(testTree.findMax(testTree.root).toString(), "7");
-        //Test 5: Als je de node met de laagste waarde zoekt, krijg je de laagste waarde terug.
-        assertEquals(testTree.findMin(testTree.root).toString(), "1");
-        //Test 6: Als je de node met de laagste waarde zoekt, krijg je niet een node met een hogere waarde terug.
-        assertNotEquals(testTree.findMin(testTree.root).toString(), "5");
+    @Test
+    public void testMethodFindNode() {
+        //Als je de node met waarde 16 zoekt, krijg je 16 terug.
+        this.test();
+        assertEquals("16", testTree.find(testTree.root, 16).toString());
+    }
 
-        testTree.insert(-5);
-        //Test 7: Na de operatie insert van een node met een lagere waarde dan de laagste waarde, krijg je de nieuwe laagste waarde terug.
-        assertEquals(testTree.findMin(testTree.root).toString(), "-5");
-        //Test 8: Na de operatie insert van een node met een lagere waarde dan de laagste waarde, krijg je de oude laagste waarde terug.
-        assertNotEquals(testTree.findMin(testTree.root).toString(), "1");
-        //Test 9: Het zoeken naar een node met een waarde die niet voorkomt, geeft null terug.
+    @Test
+    public void testMethodFindMin() {
+        //Als je de node met de laagste waarde zoekt, krijg je de laagste waarde terug.
+        this.test();
+        assertEquals("1", testTree.findMin(testTree.root).toString());
+    }
+
+    @Test
+    public void testMethodFindMax() {
+        //Als je de node met de hoogste waarde zoekt, krijg je de hoogste waarde terug.
+        this.test();
+        assertEquals("16", testTree.findMax(testTree.root).toString());
+    }
+
+    @Test
+    public void testSearchingForNotExistingNode() {
+        //Het zoeken naar een node met een waarde die niet voorkomt, geeft null terug.
+        this.test();
         assertNull(testTree.find(testTree.root, 1000));
+    }
 
+    @Test
+    public void testFindNewMin() {
+        //Na de operatie insert van een node met een lagere waarde dan de laagste waarde, krijg je de nieuwe laagste waarde terug.
+        this.test();
+        testTree.insert(-5);
+        assertEquals("-5", testTree.findMin(testTree.root).toString());
+    }
+
+    @Test
+    public void testFindNewMax() {
+        //Na de operatie insert van een node met een hogere waarde dan de hoogste waarde, krijg je de nieuwe hoogste waarde terug.
+        this.test();
+        testTree.insert(50);
+        assertEquals("50", testTree.findMax(testTree.root).toString());
     }
 }
